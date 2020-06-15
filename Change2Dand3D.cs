26af8267a4206e2b9580e6e0a58d3c4108d50078
Oleh Holota <oleggolota17@gmail.com>
2020-06-15 15:17:24 +0300

    Add scripts

---
+++ Change2Dand3D.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Change2Dand3D : MonoBehaviour
+{
+    public GameObject gameObj3D;
+    public GameObject gameObj2D;
+    bool changer;
+    float rotationModel = 60;
+    float rotationModel2 = 80;
+    public Camera androidCameraTouch;
+    public GameObject androidCameraTouchTransform;
+    Transform startPositionCamera;
+
+    private void Start()
+    {
+        startPositionCamera = androidCameraTouch.transform;
+    }
+
+    public void Change2dAnd3d()
+    {
+        
+        if (changer)
+        {
+            gameObj3D.SetActive(false);
+            gameObj2D.SetActive(true);
+            androidCameraTouch.transform.position = new Vector3(0, 70, -10);
+            androidCameraTouch.transform.rotation = Quaternion.Euler(new Vector3(88, androidCameraTouch.transform.rotation.y, androidCameraTouch.transform.rotation.z));
+        }
+        else
+        {
+            gameObj2D.SetActive(false);
+            gameObj3D.SetActive(true);
+            androidCameraTouch.transform.position = new Vector3(0, 70, -10);
+            androidCameraTouch.transform.rotation = Quaternion.Euler(new Vector3(60, androidCameraTouch.transform.rotation.y, androidCameraTouch.transform.rotation.z));
+        }
+        changer = !changer;
+    }
+}
